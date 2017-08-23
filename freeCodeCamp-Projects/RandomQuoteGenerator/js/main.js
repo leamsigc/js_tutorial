d6ae7ad3bb1bbda2407a0f41135a3ac53c8e4edc
ismael garcia <leamsigc@hotmail.com>
2017-08-22 23:02:48 -0400

    "remove fb share option "

--- freeCodeCamp-Projects/RandomQuoteGenerator/js/main.js
+++ freeCodeCamp-Projects/RandomQuoteGenerator/js/main.js
@@ -6,10 +6,12 @@ window.addEventListener('load',() => {
     btnQ.addEventListener('click', randomQuote);//click and call the random quote  function ;
     // random quote function ;
     function randomQuote () {
-        let textQuote = document.getElementById('quote-text');//text quote ;
-        // textQuote.textContent = text;
-        //quote autor ;
-        let quoteAutor = document.getElementById('quote-author');
+        const textQuote = document.getElementById('quote-text');//text quote ;
+        //quote author ;
+        const quoteAuthor = document.getElementById('quote-author');
+        //twitter share button ;
+        const twitter = document.getElementById('twitter');
+        //facebook share button ;
         //get quote api ;
         let url="https://talaikis.com/api/quotes/random/";//url api;
         //https://talaikis.com/random_quotes_api/ quote api ..;
@@ -20,11 +22,14 @@ window.addEventListener('load',() => {
             .then(data => {
                 console.log(data);//console log the data ;
                 textQuote.textContent=data["quote"];//data quote to text quote ;
-                quoteAutor.textContent=data["author"];//quote author ;
+                quoteAuthor.textContent=data["author"];//quote author ;
+                //set attribute for share twitter; 
+                twitter.setAttribute('href' , `https://twitter.com/home/?status=${data['quote']}(${data.author})`);
             })
             .catch(function(err) {//if any error we can see it in the console ;
                 // Error :(
                     console.log(err);
+                    textQuote.textContent=err;
         });
     }
 });
