73fefa8df3153c63b5c8a2975d185c79265f111f
ismael garcia <leamsigc@hotmail.com>
2017-08-30 23:08:22 -0400

    "Complet the fetch to weather api and display data "

--- freeCodeCamp-Projects/WeatherApp/js/main.js
+++ freeCodeCamp-Projects/WeatherApp/js/main.js
@@ -2,6 +2,7 @@
  //Global variables;
  let lat;
  let lng;
+ const weatherNow = [];
  //get element ;
  const $demo = document.querySelector('#demo');
  //check if geolocation supported;
@@ -24,7 +25,7 @@
 //get weather function 
 function getWeather(){
     const key = "768f10c39b6e09ced044affa134a08c6";
-    const url ="http://api.openweathermap.org/data/2.5/find?";
+    const url ="https://api.openweathermap.org/data/2.5/find?";
     const units ="metric"
     let endPoint = `${url}lat=${lat}&lon=${lng}&appid=${key}&units=${units}`;
     //fetch api weather 
@@ -32,15 +33,19 @@ function getWeather(){
         .then(data => data.json())
         .then(response => {
             console.log(response);
+            weatherNow.push(response);
             const city = response.list[0].name;
             const coordLat = response.list[0].coord.lat;
             const coordLon = response.list[0].coord.lon;
             const temp = response.list[0].main.temp;
             const humidity =response.list[0].main.humidity;
+            const icon = response.list[0].weather[0].icon;
             $demo.innerHTML=`<div class="weather_container">
                                 <div class="weather_city">${city}</div>
                                 <div class="weather_temp">${temp} &deg;C</div>
-                                <div class="weather_icon"><i class="icon-rain"></i></div>
+                                <div class="weather_icon">
+                                <img src="http://openweathermap.org/img/w/${icon}.png" >
+                                </div>
                                 <div class="weather_time">Latitude: ${coordLat}<br>Longitude:${coordLon}</div>
                                 <div class="weather_time">Humidity:${humidity}</div>
                             </div>`;
