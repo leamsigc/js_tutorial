1ad20e17c2d42ac24e00a09cfa07783414807075
ismael garcia <leamsigc@hotmail.com>
2017-09-19 15:37:50 -0400

    adding  css for the media querys and js for the response

--- freeCodeCamp-Projects/WikipediaViewer/js/main.js
+++ freeCodeCamp-Projects/WikipediaViewer/js/main.js
@@ -3,26 +3,23 @@ window.addEventListener('load', function (){
 const input = document.getElementById('search-input');
 console.log(input);
     input.addEventListener('keydown', function(e){
-        console.log(e.keyCode);
         if(e.keyCode === 13){
             let inputValue = input.value;
             let removeSpace= inputValue.split(' ').join('+');
-
-            console.log(removeSpace);
             wikiViewer(removeSpace);
+            input.value = "";
         }
     });
     function wikiViewer(value) {
         let app = document.getElementById('app');
         let endPoint = `http://cors.io/?https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${value}`;
-        console.log(endPoint);
        axios.get(endPoint)
        .then(response =>{
         let searchResults = response.data.query.search;
         let display = '<ul>';
         searchResults.forEach(function(element) {
             display+=
-            `<li>
+            `<li class="wiki_results">
                 <h2>${element.title}</h2>
                 <a href='https://en.wikipedia.org/?curid=${element.pageid}'target='_blank'>
                 <p>${element.snippet}</p>
