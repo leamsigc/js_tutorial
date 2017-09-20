487142557494bc6a6595053232f8078a0ff3a6d4
ismael garcia <leamsigc@hotmail.com>
2017-09-20 00:45:36 -0400

    make the user interface add the logo twich ,input  and buton to filter live and offline

--- freeCodeCamp-Projects/TwichtTvAPI/js/main.js
+++ freeCodeCamp-Projects/TwichtTvAPI/js/main.js
@@ -1,5 +1,23 @@
 // window load 
 window.addEventListener('load', function () {
     'use strict';
-    console.log('we are connected');
+    checkLive(userNames[0]);
 });
+const userNames = ["ESL_SC2", "OgamingSC2",  "freecodecamp", "RobotCaleb", "comster404", "brunofin"];
+//function to call api 
+function checkLive (strimerName){
+    let endPointChannel =`https://cors.io/?https://wind-bow.gomix.me/twitch-api/channels/${strimerName}`;
+    console.log(endPointChannel);
+    axios.get(endPointChannel)
+        .then(response => 
+        {
+            console.log(response.data);
+        })
+        .catch(error => console.log(error));
+    //check user live Stream 
+    let endPointUser = `https://cors.io/?https://wind-bow.gomix.me/twitch-api/streams/${strimerName}`;
+    console.log(endPointUser);   
+    axios.get(endPointUser)
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+}
