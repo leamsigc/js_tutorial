0b4292bfaef5bdb0874a659ee4f50087c8a61942
ismael garcia <leamsigc@hotmail.com>
2017-09-21 06:45:20 -0400

    change color of the fa icon when the channel is online

--- freeCodeCamp-Projects/TwichtTvAPI/js/main.js
+++ freeCodeCamp-Projects/TwichtTvAPI/js/main.js
@@ -1,23 +1,37 @@
 // window load 
 window.addEventListener('load', function () {
     'use strict';
-    checkLive(userNames[0]);
-});
-const userNames = ["ESL_SC2", "OgamingSC2",  "freecodecamp", "RobotCaleb", "comster404", "brunofin"];
-//function to call api 
-function checkLive (strimerName){
+    const userNames = ["ESL_SC2", "OgamingSC2",  "freecodecamp", "RobotCaleb", "comster404", "brunofin"];
+    const $display = document.getElementById('app');
+    userNames.forEach(item =>{
+        checkLive(item, $display);
+    })
+});//function to call api 
+function checkLive (strimerName,app){
+    let ul = "";
     let endPointChannel =`https://cors.io/?https://wind-bow.gomix.me/twitch-api/channels/${strimerName}`;
     console.log(endPointChannel);
     axios.get(endPointChannel)
         .then(response => 
         {
+            ul +=`<li  class="user-info">
+            <img src="${response.data.profile_banner}">
+            <a href="${response.data.url}" target="_blanc">${response.data.name}</a>`;
             console.log(response.data);
+            //check user live Stream 
+            let endPointUser = `https://cors.io/?https://wind-bow.gomix.me/twitch-api/streams/${strimerName}`;
+            console.log(endPointUser);   
+            axios.get(endPointUser)
+            .then(userResponse => {
+                console.log("===========================");
+                console.log(userResponse)
+                if(userResponse.data.stream === null){
+                    ul+=`<i class="fa fa-ban fa-2x" aria-hidden="true"></i>`;
+                }else{
+                    ul+=`<i class="fa fa-check fa-2x" aria-hidden="true"></i>`;}
+                console.log("===========================");
+                app.innerHTML+=ul;
+            }).catch(error => console.log(error));
         })
         .catch(error => console.log(error));
-    //check user live Stream 
-    let endPointUser = `https://cors.io/?https://wind-bow.gomix.me/twitch-api/streams/${strimerName}`;
-    console.log(endPointUser);   
-    axios.get(endPointUser)
-    .then(response => console.log(response))
-    .catch(error => console.log(error));
 }
