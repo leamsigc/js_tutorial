b7c90ec35209cf6959114ccf5091952ee29b148a
ismael <leamsigc@hotmail.com>
2017-12-17 22:35:46 -0500

    ennable jslint for es6 syntax

---
+++ LoanCalculator/app.js
@@ -0,0 +1,5 @@
+/*jslint es6 */
+// ui variables
+const $amount = document.querySelector('#amount');
+const $interest = document.querySelector('#interest');
+const $yeas = document.querySelector('#years');
