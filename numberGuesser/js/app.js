016a417c85809dda703d7c1cde889179ba266465
ismael <leamsigc@hotmail.com>
2017-12-21 15:14:32 -0500

    adjust the space bettwen the files

---
+++ numberGuesser/js/app.js
@@ -0,0 +1,8 @@
+/**
+ * GAME FUNCTION
+ * Player must guess a number between a min and max
+ * get a certain number of guesses
+ * Notify player of remaining guesses
+ * Notify the player of correct answer if loose
+ * Let player choose to play again
+ */
