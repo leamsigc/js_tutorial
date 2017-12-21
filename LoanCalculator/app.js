9e25b3f7c7d49b9d828b6fb3b058f052005b9898
ismael <leamsigc@hotmail.com>
2017-12-21 00:22:50 -0500

    remove the event in the parameter of the calculate function

--- LoanCalculator/app.js
+++ LoanCalculator/app.js
@@ -14,7 +14,7 @@ $form.addEventListener('submit', function (e) {
     e.preventDefault();
 });
 
-function calculate(e) {
+function calculate() {
     // ui elements
     const $amount = document.querySelector('#amount');
     const $interest = document.querySelector('#interest');
@@ -29,17 +29,17 @@ function calculate(e) {
     const principal = parseFloat($amount.value); //convert string to numbers
     const calculatedInterest = parseFloat($interest.value) / 100 / 12;
     const calculatePayments = parseFloat($yeas.value) * 12;
-    console.log(`this is the ${principal}  calculate interes${calculatedInterest}`);
+    // console.log(`this is the ${principal}  calculate interest${calculatedInterest}`);
     //calculate monthly payments
     const x = Math.pow(1 + calculatedInterest, calculatePayments);
-    console.log(x);
+    // console.log(x);
     const monthly = (principal * x * calculatedInterest) / (x - 1);
     if (isFinite(monthly)) {
 
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatePayments).toFixed(2);
         totalInterest.value = ((monthly * calculatePayments) - principal).toFixed(2);
-        //hide spiner
+        //hide spinner
         document.getElementById('loading').style.display = 'none';
         //show results
         document.getElementById('result').style.display = 'block';
@@ -71,7 +71,7 @@ function showError(error) {
     errorDiv.appendChild(document.createTextNode(error));
     //insert before the heading;
     card.insertBefore(errorDiv, h2);
-    //clear error 
+    //clear error
     setTimeout(clearError, 2000);
 }
 
